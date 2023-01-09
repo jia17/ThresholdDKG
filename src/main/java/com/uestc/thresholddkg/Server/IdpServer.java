3bbec3f6c9e11d4085c86158d5ab418f2257e7cc
keven <keven.jia.zhang@qq.com>
2023-01-09 19:02:03 +0800

    Revert "clear some"
    This reverts commit 789acdc98c318ad0c61314b9cb9ff35b596700e8.

--- src/main/java/com/uestc/thresholddkg/Server/IdpServer.java
+++ src/main/java/com/uestc/thresholddkg/Server/IdpServer.java
@@ -46,6 +46,8 @@ public class IdpServer  implements ApplicationListener<ContextRefreshedEvent> {
     private static String[] addrS;
     @Value("#{'${idpservers.ipandport.ServersIp}'.split(' ')}")
     private String[] configAddr;
+    private static String[] IPs;
+    private static Integer[] Ports;
 
     private static HttpsServer[] servers;
 
@@ -62,8 +64,6 @@ public class IdpServer  implements ApplicationListener<ContextRefreshedEvent> {
 //        var s2=  JSONObject.toBean(jsonobject,TestConv.class);
 //        TestConv res=(TestConv) convert.Json2obj(ss);
 //        System.out.println(res.getText()[0]);
-        String[] IPs;
-        Integer[] Ports;
         IPs=new String[serverNum];
         Ports=new Integer[serverNum];
         String[] addrSplit;int i=0;
